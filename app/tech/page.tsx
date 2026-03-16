4693d5a6a4ad9eeeb56f2edf239a51f4d0dc9ae9
10ra <jaymarcgososo@gmail.com>
2026-03-16 22:26:28 +0800

    Add: bg-image, globals.css

--- app/tech/page.tsx
+++ app/tech/page.tsx
@@ -1,84 +1,41 @@
 import { Button } from "@/components/ui/button"
-import backgroundImage from "@/public/technology-assets/background-technology-desktop.jpg"
-import backgroundImageTablet from "@/public/technology-assets/background-technology-tablet.jpg"
-import backgroundImageMobile from "@/public/technology-assets/background-technology-mobile.jpg"
+import React from "react";
 
 export default function Tech() {
   return (
     <>
-      {/* Mobile - max-width 420px */}
-      <div 
-        className="flex md:hidden min-h-svh p-6"
-        style={{
-          backgroundImage: `url(${backgroundImageMobile.src})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-          backgroundAttachment: 'fixed',
-          maxWidth: '420px'
-        }}
-      >
-        <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-          <div>
-            <h1 className="font-medium">Project readyxxx!</h1>
-            <p>You may now add components and start building.</p>
-            <p>We&apos;ve already added the button component for you.</p>
-            <Button className="mt-2">Button</Button>
-          </div>
-          <div className="font-mono text-xs text-muted-foreground">
-            (Press <kbd>d</kbd> to toggle dark mode)
-          </div>
-        </div>
+ 
+    {/* Navigation Bar */}
+    <nav
+    className="top-0 absolute flex justify-between bg-transparent text-white items-center w-full mt-5 md:mt-0 lg:mt-12 z-30">
+      <div
+      className="mx-5 w-8 h-8 md:w-10 md:h-10 lg:w-14 lg:h-14 lg:mx-12 cursor-pointer>">
+        
       </div>
+    </nav>
 
-      {/* Tablet - max-width 769px */}
-      <div 
-        className="hidden md:flex lg:hidden min-h-svh p-6"
-        style={{
-          backgroundImage: `url(${backgroundImageTablet.src})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-          backgroundAttachment: 'fixed',
-          maxWidth: '769px'
-        }}
-      >
-        <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-          <div>
-            <h1 className="font-medium">Project readyxxxx!</h1>
-            <p>You may now add components and start building.</p>
-            <p>We&apos;ve already added the button component for you.</p>
-            <Button className="mt-2">Button</Button>
-          </div>
-          <div className="font-mono text-xs text-muted-foreground">
-            (Press <kbd>d</kbd> to toggle dark mode)
-          </div>
-        </div>
-      </div>
+    <div
+    className="hidden  z-10 right-0 h-screen backdrop-blur-[24px] bg-black/80 fixed text-white px-8 py-8 top-0">
 
-      {/* Desktop - no max-width */}
-      <div 
-        className="hidden lg:flex min-h-svh p-6"
-        style={{
-          backgroundImage: `url(${backgroundImage.src})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-          backgroundAttachment: 'fixed'
-        }}
-      >
-        <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-          <div>
-            <h1 className="font-medium">Project ready!xxxxx</h1>
-            <p>You may now add components and start building.</p>
-            <p>We&apos;ve already added the button component for you.</p>
-            <Button className="mt-2">Button</Button>
-          </div>
-          <div className="font-mono text-xs text-muted-foreground">
-            (Press <kbd>d</kbd> to toggle dark mode)
-          </div>
-        </div>
-      </div>
+    </div>
+
+{/* Background Image  */}
+  <div
+    className="bg-tech-mobile md:bg-tech-tablet lg:bg-tech-desktop bg-cover flex flex-col h-screen md:justify-around pt-20 md:pt-28 pb-12 lg:pl-28">
+
+    <div
+       className="flex text-white tracking-wider barlow-condensed mb-6 md:mb-10 justify-center md:justify-start md:ml-5 md:text-xl lg:pt-12 lg:text-2x1"> 
+       <span className="opacity-25 mr-4 bold">03</span>
+       <p> SPACE LAUNCH 101 </p>
+       </div>
+
+    </div>
+
+   
+
+ 
     </>
+
+ 
   )
 }
