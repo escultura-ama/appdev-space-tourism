6b000c97ad1af89d2e2d6e96420d964f787e786a
Blackdeniel360 <saurirenren@gmail.com>
2026-03-24 18:31:49 +0800

    Tablet layout fixed!

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,9 @@
-import { Geist, Geist_Mono, Barlow_Condensed, Bellefair } from "next/font/google"
+import { Barlow_Condensed, Bellefair } from "next/font/google"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils";
 
-const fontSans = Geist({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
-
-const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
-})
 
 const barlowCondensed = Barlow_Condensed({
   subsets: ["latin"],
@@ -35,7 +26,7 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, fontSans.variable, barlowCondensed.variable, bellefair.variable, "font-sans")}
+      className={cn("antialiased", barlowCondensed.variable, bellefair.variable, "font-sans")}
     >
       <body>
         <ThemeProvider>{children}</ThemeProvider>
--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,19 @@
     <div className="bg-home-bg relative min-h-screen flex flex-col overflow-x-hidden">
       
       {/* Main Container */}
-      <div className="flex-1 flex flex-col lg:flex-row items-center lg:items-end justify-between px-6 md:px-40 lg:px-36 pb-24 lg:pb-32 md:mb-16 md:ml-18">
+      <div className="flex-1 flex flex-col lg:flex-row items-center lg:items-end justify-between px-6 md:px-40 lg:px-36 pb-24 lg:pb-32 md:mb-16 md:ml-18 md:mr-18">
         
         {/* Left Content */}
         <div className="flex flex-col items-center text-center lg:items-start lg:text-left w-full lg:max-w-[540px] mt-24 md:mt-48 lg:mt-10">
-          <h1 className="font-barlow condensed,sans serif text-primaryText tracking-wider font-normal text-[px] md:text-xl lg:text-3xl lg:text-widest uppercase">
+          <h1 className="font-barlow condensed,sans-serif text-primaryText tracking-wider md:text-xl lg:text-3xl lg:text-widest uppercase">
             So, you want to travel to
           </h1>
           
-          <h2 className="font-bellefair font-normal text-white text-[80px] md:text-[170px] leading-[100px] md:leading-[150px] lg:leading-[172px] mt-4 md:mt-6 uppercase lg:mb-10">
+          <h2 className="font-bellefair text-6xl text-white text-[80px] md:text-[130px] lg:text-[175px] leading-[100px] md:leading-[150px] lg:leading-[172px] mt-6 mb-10 md:mb-12 md:text-9xl uppercase">
             Space
           </h2>
           
-         <p className="font-barlow,sans-serif text-primaryText text-[15px] md:text-[20px] lg:text-[20px] leading-[25px] md:leading-[28px] lg:leading-[32px] mt-4 md:mt-6 mx-auto lg:mx-0 max-w-[327px] md:max-w-[444px] lg:max-w-none">
+         <p className="font-barlow text-primaryText text-[20px] md:text-[20px] lg:text-[25px] leading-[25px] md:leading-[28px] lg:leading-[32px] md:mt-6 mx-auto lg:mx-0 max-w-[327px] md:max-w-[444px] lg:max-w-none wordSpacing-[3px] letterspacing-[5px]">
   Let’s face it; if you want to go to space, you might as well{" "}
   <span className="lg:whitespace-nowrap">
     genuinely go to outer
@@ -27,7 +27,7 @@
         </div>
 
         {/* Right Button (Explore) */}
-        <div className="mt-20 md:mt-40 lg:mt-0 flex items-center justify-center">
+        <div className="flex md:ml-10  mt-20 md:mt-40 lg:mt-0 flex items-center justify-between">
           <button
             className="group relative w-40 h-40 md:w-64 md:h-64 lg:w-62 lg:h-62 rounded-full bg-white text-black text-[20px] md:text-[31px] font-bellefair uppercase tracking-[1.25px] md:tracking-[1px] transition-all duration-500 flex items-center justify-center cursor-pointer md:mr-5 md:mb-23"
           >
